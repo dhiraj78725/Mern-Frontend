d05540a497a0eb09609093caa3de5c16ff7ae76d
dhiraj.empeal <dhiraj.bhardwaj@empeal.com>
2023-05-01 19:13:48 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Footer() {
+    return(
+        <div className="footer">
+            <h3>E-Commerce Dashboard</h3>
+            
+        </div>
+    )
+    
+}
